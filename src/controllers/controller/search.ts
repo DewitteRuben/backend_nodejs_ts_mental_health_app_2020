69c7569a45ca604edbd1b23686a3ff9e6634143d
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-05-08 22:04:19 +0200

    refactor: renamed generic controller

--- src/controllers/controller/search.ts
+++
@@ -1,6 +0,0 @@
-import { RequestHandler } from "express";
-import handleErrorMiddleware from "../../middleware/handle-error-middleware";
-
-const get: RequestHandler = async (req, res) => {};
-
-export default handleErrorMiddleware(get);
