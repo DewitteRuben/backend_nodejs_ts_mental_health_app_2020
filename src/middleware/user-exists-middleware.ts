6f1aa46d99f3fd617ddbd3a3fbb94fc4a89ad1b5
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-05-10 08:20:33 +0200

    feat: user exists middleware

---
+++ src/middleware/user-exists-middleware.ts
@@ -0,0 +1,13 @@
+import { RequestHandler } from "express";
+import verifyTokenMiddleware from "./verify-token-middleware";
+import { exists } from "../utils/user";
+import { ApplicationError } from "../errors";
+
+const userExistsMW: RequestHandler = async (req, res, next) => {
+  const { userId } = req.body.auth;
+  const doesUserExist = await exists(userId);
+  if (!doesUserExist) throw new ApplicationError("No user was found that matches the userId", 401);
+  return next();
+};
+
+export default userExistsMW;
