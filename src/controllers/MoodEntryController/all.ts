059c5f38c2eb45ca08b45aa63693d07f10a3179e
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-05-17 12:14:59 +0200

    feat: added req handler to find entries by id

--- src/controllers/MoodEntryController/all.ts
+++ src/controllers/MoodEntryController/all.ts
@@ -1,6 +1,13 @@
 import { RequestHandler } from "express";
 import handleErrorMiddleware from "../../middleware/handle-error-middleware";
+import MoodEntry from "../../models/MoodEntry";
 
-const all: RequestHandler = async (req, res) => {};
+const all: RequestHandler = async (req, res) => {
+  const { userId } = req.body.auth;
+
+  const moodEntries = await MoodEntry.find({ userId });
+
+  return res.json(moodEntries);
+};
 
 export default handleErrorMiddleware(all);
