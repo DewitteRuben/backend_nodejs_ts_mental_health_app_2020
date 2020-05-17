d6d317526ad5d9aa5f074ca642f905a606e754ff
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-05-17 12:14:03 +0200

    feat: added database check in userExists middleware

--- src/middleware/user-exists-middleware.ts
+++ src/middleware/user-exists-middleware.ts
@@ -5,8 +5,17 @@ import { ApplicationError } from "../errors";
 
 const userExistsMW: RequestHandler = async (req, res, next) => {
   const { userId } = req.body.auth;
+
+  if (!userId) {
+    throw new ApplicationError("No valid userId was found in the authorization header.");
+  }
+
   const doesUserExist = await exists(userId);
-  if (!doesUserExist) throw new ApplicationError("No user was found that matches the userId", 401);
+
+  if (!doesUserExist) {
+    throw new ApplicationError("No user was found that matches the userId", 401);
+  }
+
   return next();
 };
 
