458f5f7d4f50db01cc28303ff11bc1f966d288c9
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-05-23 09:03:13 +0200

    feat: filter results with entryId query param

--- src/controllers/MoodEntryController/all.ts
+++ src/controllers/MoodEntryController/all.ts
@@ -3,9 +3,10 @@ import handleErrorMiddleware from "../../middleware/handle-error-middleware";
 import MoodEntry from "../../models/MoodEntry";
 
 const all: RequestHandler = async (req, res) => {
+  const { entryId } = req.query;
   const { userId } = req.body.auth;
 
-  const moodEntries = await MoodEntry.find({ userId });
+  const moodEntries = await MoodEntry.find({ userId, ...(entryId && { entryId }) });
 
   return res.json(moodEntries);
 };
