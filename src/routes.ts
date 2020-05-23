7f2b5eafe28dc3a3edfcef248d8a85c4d1e8855d
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-05-23 09:04:39 +0200

    feat: init new routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,22 @@
 import { Router } from "express";
 import swaggerUi from "swagger-ui-express";
 import apiSpec from "../openapi.json";
+import * as AuthController from "./controllers/AuthController";
+import * as MoodEntryController from "./controllers/MoodEntryController";
+import * as UserController from "./controllers/UserController";
+import verifyUserMiddleware from "./middleware/verify-user-middleware";
 
 const swaggerUiOptions = {
   customCss: ".swagger-ui .topbar { display: none }"
 };
 
 const router = Router();
+
+router.post("/auth", AuthController.auth);
+router.post("/user", UserController.add);
+router.get("/moodentry", verifyUserMiddleware, MoodEntryController.filter);
+router.get("/moodentry", verifyUserMiddleware, MoodEntryController.all);
+router.post("/moodentry", verifyUserMiddleware, MoodEntryController.add);
 
 // Dev routes
 if (process.env.NODE_ENV === "development") {
