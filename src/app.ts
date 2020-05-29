d8b873aa5cb7c8dc40805b84bccad4dc58d89828
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-05-29 13:33:54 +0200

    feat: added cors mw

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import bodyParser from "body-parser";
 import compression from "compression";
 import express, { NextFunction, Request, Response } from "express";
 import path from "path";
+import cors from "cors";
 import { ApplicationError } from "./errors";
 import routes from "./routes";
 
 const app = express();
 
+app.use(cors());
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
