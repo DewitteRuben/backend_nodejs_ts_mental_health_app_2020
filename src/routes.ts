6d4958e5937746c938cd333b28445435125edd0a
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-06-01 00:00:11 +0200

    feat: updated routes

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import * as AuthController from "./controllers/AuthController";
 import * as MoodEntryController from "./controllers/MoodEntryController";
 import * as ProfessionalController from "./controllers/ProfessionalController/index";
 import verifyUserMiddleware from "./middleware/verify-user-middleware";
+import verifyProfessionalMiddleware from "./middleware/verify-professional-middleware";
 
 const swaggerUiOptions = {
   customCss: ".swagger-ui .topbar { display: none }"
@@ -14,9 +15,11 @@ const router = Router();
 
 router.post("/auth/user", AuthController.auth);
 router.post("/auth/professional", AuthController.authProfessional);
+router.post("/auth/professional/refresh", AuthController.refresh);
 
 router.post("/professional", ProfessionalController.add);
-router.post("/user", ProfessionalController.registerUser);
+router.post("/user", verifyProfessionalMiddleware, ProfessionalController.registerUser);
+router.delete("/user", verifyProfessionalMiddleware, ProfessionalController.deleteClient);
 
 router.get("/moodentry", verifyUserMiddleware, MoodEntryController.filter);
 router.get("/moodentry", verifyUserMiddleware, MoodEntryController.all);
