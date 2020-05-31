ff17819a3e164900aaa9300d3f9c38eafb1ab963
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-05-31 23:57:32 +0200

    feat: helper to populate professional data with users

--- src/utils/professional.ts
+++ src/utils/professional.ts
@@ -1,5 +1,11 @@
 import Professional from "../models/Professional";
+import User from "../models/User";
 
 const exists = async (email: string) => Professional.exists({ email });
 
-export { exists };
+const populateClientIds = async (clients: string[]) => {
+  const clientDetails = await User.find().where("userId").in(clients).exec();
+  return clientDetails.map((user) => user.toJSON());
+};
+
+export { exists, populateClientIds };
